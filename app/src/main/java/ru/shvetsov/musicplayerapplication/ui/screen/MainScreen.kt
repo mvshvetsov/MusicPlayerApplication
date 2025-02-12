6ddfe037f80f88fc5dc6e071c1ddb05f4f93613f
Matvey Shvetsov <matvey.shvetsov@bk.ru>
2025-02-12 22:14:35 +0500

    Added BottomNavigation

---
+++ app/src/main/java/ru/shvetsov/musicplayerapplication/ui/screen/MainScreen.kt
@@ -0,0 +1,21 @@
+package ru.shvetsov.musicplayerapplication.ui.screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ru.shvetsov.musicplayerapplication.navigation.BottomNavigationBar
+import ru.shvetsov.musicplayerapplication.navigation.NavGraph
+
+@Composable
+fun MainScreen(navController: NavHostController = rememberNavController()) {
+    Scaffold(
+        containerColor = Color.White,
+        bottomBar = { BottomNavigationBar(navController = navController) }
+    ) { paddingValues ->
+        NavGraph(navHostController = navController, modifier = Modifier.padding(paddingValues))
+    }
+}
