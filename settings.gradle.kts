1a1ed2ec74a3a0ab1c6ee3086c9d1c7bbca7d5f9
Matvey Shvetsov <matvey.shvetsov@bk.ru>
2025-02-12 19:33:41 +0500

    Initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MusicPlayerApplication"
+include(":app")
+include(":common")
+include(":local_music_feature")
+include(":local_music_feature:data")
+include(":local_music_feature:domain")
+include(":local_music_feature:presentation")
+include(":remote_music_feature")
+include(":remote_music_feature:data")
+include(":remote_music_feature:domain")
+include(":remote_music_feature:presentation")
+include(":music_player_feature")
+include(":track_list_common")
