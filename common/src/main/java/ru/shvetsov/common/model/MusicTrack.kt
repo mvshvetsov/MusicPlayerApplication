bde7d00c64596a08244a51c0b61e6d7cd9f607ca
Matvey Shvetsov <matvey.shvetsov@bk.ru>
2025-02-15 00:17:05 +0500

    Delete fields

--- common/src/main/java/ru/shvetsov/common/model/MusicTrack.kt
+++ common/src/main/java/ru/shvetsov/common/model/MusicTrack.kt
@@ -4,8 +4,5 @@ data class MusicTrack(
     val id: Long?,
     val title: String?,
     val artist: String?,
-    val cover: String?,
-    val duration: Long?,
-    val albumTitle: String?,
-    val trackPath: String?
+    val cover: String?
 )
