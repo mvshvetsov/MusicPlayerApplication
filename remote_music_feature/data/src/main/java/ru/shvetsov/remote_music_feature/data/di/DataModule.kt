a2a78360723d461070f8d2f240ab44f9ee92b461
Matvey Shvetsov <matvey.shvetsov@bk.ru>
2025-02-14 22:00:12 +0500

    Retrofit DI removed

--- remote_music_feature/data/src/main/java/ru/shvetsov/remote_music_feature/data/di/DataModule.kt
+++ remote_music_feature/data/src/main/java/ru/shvetsov/remote_music_feature/data/di/DataModule.kt
@@ -4,10 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import ru.shvetsov.remote_music_feature.data.remote.RemoteMusicTracksService
 import ru.shvetsov.remote_music_feature.data.repository.RemoteMusicTracksRepositoryImpl
 import ru.shvetsov.remote_music_feature.domain.repository.RemoteMusicTracksRepository
@@ -16,30 +13,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object DataModule {
-
-    private const val BASE_URL = "https://api.deezer.com/"
-
-    @Provides
-    @Singleton
-    fun providesLoggingInterceptor() = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
-
-    @Provides
-    @Singleton
-    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor) =
-        OkHttpClient.Builder()
-            .addInterceptor(httpLoggingInterceptor)
-            .build()
-
-    @Provides
-    @Singleton
-    fun providesRetrofitClient(okHttpClient: OkHttpClient): Retrofit =
-        Retrofit.Builder()
-            .client(okHttpClient)
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
     @Provides
     @Singleton
